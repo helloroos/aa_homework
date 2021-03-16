91d5f7d52b0106ab38e583b7f01323e20d7e4dc1
michelleroos <hello.michelleroos@gmail.com>
2021-03-16 08:42:44 -0400

    Worked on make_move

--- W4D2/mancala/lib/board.rb
+++ W4D2/mancala/lib/board.rb
@@ -20,10 +20,21 @@ class Board
   def valid_move?(start_pos)
     raise "Invalid starting cup" if start_pos < 0 || start_pos > 13
     raise "Starting cup is empty" if @cups[start_pos].length == 0
-
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    until stones.empty?
+      stone = stones.shift
+      next_cup_idx = (start_pos += 1) % 13
+      @cups[next_cup_idx] << stone
+    end
+    render
+    ending_cup_idx = (next_cup_idx -= 1) % 13
+    next_turn(ending_cup_idx)
+    return :switch if @cups[ending_cup_idx].empty?
   end
 
   def next_turn(ending_cup_idx)
